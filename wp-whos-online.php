5c0bf5ce3a7581d895686ea345cfdbeb725e07c6
AdamBackstrom <AdamBackstrom@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-03 14:26:01 +0000

    Add a license.
    git-svn-id: https://plugins.svn.wordpress.org/wp-whos-online/trunk@446744 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-whos-online.php
+++ wp-whos-online.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 Plugin Name: Who's Online
 Plugin URI: http://www.plymouth.edu/
@@ -7,6 +6,23 @@ Description: Sidebar widget to log when a user was last online
 Version: 0.1
 Author: Adam Backstrom
 Author URI: http://blogs.bwerp.net/
+License: GPL2
+*/
+
+/*  Copyright 2011  Adam Backstrom <adam@sixohthree.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 function wpwhosonline_init() {
